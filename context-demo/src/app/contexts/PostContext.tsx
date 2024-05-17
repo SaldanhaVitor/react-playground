3af707b2f67c6d22156139a3b1e9a2cfb195b6c8
SaldanhaVitor <vitorgtfo@gmail.com>
2024-05-17 20:09:02 -0300

    refactor: add localStorage to persist posts

--- context-demo/src/app/contexts/PostContext.tsx
+++ context-demo/src/app/contexts/PostContext.tsx
@@ -1,6 +1,8 @@
-import { Dispatch, ReactNode, createContext, useContext, useReducer } from "react";
+import { Dispatch, ReactNode, createContext, useContext, useEffect, useReducer, useState } from "react";
 import { Post } from "../types/Post.type";
-import { PostActions, PostEnum, PostReducer } from "../reducers/PostReducer";
+import { PostActions, PostReducer } from "../reducers/PostReducer";
+
+const STORAGE_KEY = 'postContextContent';
 
 type PostContextType = {
   posts: Post[]
@@ -9,7 +11,22 @@ type PostContextType = {
 export const PostContext = createContext<PostContextType | null>(null)
 
 export const PostProvider = ({ children }: { children: ReactNode }) => {
-  const [posts, dispatch] = useReducer(PostReducer, [])
+  const [isMounted, setIsMounted] = useState(false);
+  const [posts, dispatch] = useReducer(
+    PostReducer,
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]")
+      : []
+  )
+
+  useEffect(() => {
+    setIsMounted(true);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(posts));
+  }, [posts]);
+
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <PostContext.Provider value={{ posts, dispatch }} >{children}</PostContext.Provider>
